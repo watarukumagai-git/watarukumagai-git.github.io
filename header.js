237f2cca8cf6d7ddf51ece6963921f3951361af6
watarukumagai-git <90257167+watarukumagai-git@users.noreply.github.com>
2023-05-01 23:51:12 +0900

    Update header.js

--- header.js
+++ header.js
@@ -1,7 +1,8 @@
-fetch("header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-  });
+function include_header(){
+    $.ajax({
+        url: 'header.html', // リクエストを送信するURLを指定
+        async: false, // 非同期リクエストを無効にする
+    }).done(function(header_html){ // 通信が成功したら
+        document.write(header_html); // herder.htmlを表示する
+    });
+}
