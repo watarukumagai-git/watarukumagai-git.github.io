0a1957f78856bcba3f46c8e10f8eb7a6224ae41c
watarukumagai-git <90257167+watarukumagai-git@users.noreply.github.com>
2023-05-02 00:51:08 +0900

    Delete header.js

--- header.js
+++
@@ -1,8 +0,0 @@
-function include_header(){
-    $.ajax({
-        url: 'header.html', // リクエストを送信するURLを指定
-        async: false, // 非同期リクエストを無効にする
-    }).done(function(header_html){ // 通信が成功したら
-        document.write(header_html); // herder.htmlを表示する
-    });
-}
